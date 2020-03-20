63de0520eb0c82342c46b76c3dc2fee68b5067f9
Nivo1985 <karol.rogowski@gmail.com>
2020-03-20 22:02:37 +0100

    Add files via upload

---
+++ JavaScript Patterns/4) Factory/Factory.js
@@ -0,0 +1,104 @@
+class Car {
+  constructor(options) {
+    this.doors = options.doors || 4;
+    this.state = options.state || "brand new";
+    this.color = options.color || "white";
+  }
+}
+class Truck {
+  constructor(options) {
+    this.doors = options.doors || 4;
+    this.state = options.state || "used";
+    this.color = options.color || "black";
+  }
+}
+class VehicleFactory {
+  createVehicle(options) {
+    // creation conditions
+    if (options.vehicleType === "car") {
+      return new Car(options);
+    } else if (options.vehicleType === "truck") {
+      return new Truck(options);
+    }
+  }
+}
+
+const factory = new VehicleFactory();
+const car = factory.createVehicle({
+  vehicleType: "car",
+  doors: 4,
+  color: "silver",
+  state: "Brand New"
+});
+const truck = factory.createVehicle({
+  vehicleType: "truck",
+  doors: 2,
+  color: "white",
+  state: "used"
+});
+
+console.log(car);
+
+console.log(truck);
+console.log(
+  //   class accesable. Good idea?
+  new Car({
+    doors: "Yes",
+    color: 666,
+    state: Date.now()
+  })
+);
+
+// // 2
+// const factory = (function() {
+//   class Car {
+//     constructor(options) {
+//       this.doors = options.doors || 4;
+//       this.state = options.state || "brand new";
+//       this.color = options.color || "white";
+//     }
+//   }
+//   class Truck {
+//     constructor(options) {
+//       this.doors = options.doors || 4;
+//       this.state = options.state || "used";
+//       this.color = options.color || "black";
+//     }
+//   }
+
+//   return {
+//     createVehicle: options => {
+//       if (options.vehicleType === "car") {
+//         return new Car(options);
+//       } else if (options.vehicleType === "truck") {
+//         return new Truck(options);
+//       }
+//     }
+//   };
+// })();
+
+// const car = factory.createVehicle({
+//   vehicleType: "car",
+//   doors: 4,
+//   color: "silver",
+//   state: "Brand New"
+// });
+// const truck = factory.createVehicle({
+//   vehicleType: "truck",
+//   doors: 2,
+//   color: "white",
+//   state: "used"
+// });
+
+// console.log(car);
+
+// console.log(truck);
+
+// console.log(
+//   //   class accesable. Good idea?
+//   new Car({
+//     doors: "Yes",
+//     color: 666,
+//     state: Date.now()
+//   })
+// );
