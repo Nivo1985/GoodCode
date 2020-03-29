07746bf53d0c8a1aec6ea9b53df4a59318fdaf8f
Nivo1985 <karol.rogowski@gmail.com>
2020-03-29 20:27:37 +0200

    Add files via upload

---
+++ JavaScript Patterns/5)Observer/Observer.js
@@ -0,0 +1,39 @@
+function Show1() {
+  console.log("Show 1");
+}
+
+function Show2(x) {
+  console.log("Show 2. Arg " + x);
+}
+
+function Show3(x, y, z) {
+  console.log("Show 3. Arg " + x + ", " + y + ", " + z);
+}
+
+class Subscriber {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(func) {
+    this.observers.push(func);
+  }
+  unsubscribe(func) {
+    this.observers = this.observers.filter(fn => fn !== func);
+  }
+  fire(...arg) {
+    this.observers.forEach(fn => fn(...arg));
+  }
+}
+
+let subscriber = new Subscriber();
+subscriber.subscribe(Show1);
+subscriber.subscribe(Show2);
+subscriber.subscribe(Show2);
+subscriber.subscribe(Show3);
+
+subscriber.fire();
+subscriber.fire("a", "b", "c");
+
+subscriber.unsubscribe(Show2);
+subscriber.fire("a", "b", "c");
